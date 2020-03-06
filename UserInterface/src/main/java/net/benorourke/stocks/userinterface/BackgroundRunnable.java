b4b0a28a6ba1bd4c1a74411c2e7ccef37d6fbf7b
Ben O'Rourke <benorourke@users.noreply.github.com>
2020-03-06 23:09:34 +0000

    Decouple UI Thread & Background Thread(s)

---
+++ UserInterface/src/main/java/net/benorourke/stocks/userinterface/BackgroundRunnable.java
@@ -0,0 +1,9 @@
+package net.benorourke.stocks.userinterface;
+
+import net.benorourke.stocks.framework.Framework;
+
+public interface BackgroundRunnable {
+
+        void run(Framework framework);
+
+}
--- UserInterface/src/main/java/net/benorourke/stocks/userinterface/BackgroundThread.java
+++ UserInterface/src/main/java/net/benorourke/stocks/userinterface/BackgroundThread.java
@@ -3,19 +3,58 @@ package net.benorourke.stocks.userinterface;
 import net.benorourke.stocks.framework.Configuration;
 import net.benorourke.stocks.framework.Framework;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class BackgroundThread extends Thread
 {
     private final Framework framework;
+    private final BlockingQueue<BackgroundRunnable> runnables;
+    private final long sleepDelay;
 
-    public BackgroundThread(Configuration configuration)
+    public BackgroundThread(Configuration configuration, long sleepDelay)
     {
         this.framework = new Framework(configuration);
+        this.runnables = new LinkedBlockingQueue<>();
+        this.sleepDelay = sleepDelay;
     }
 
     @Override
     public void run()
     {
+        while (true)
+        {
+            StockApplication.debug("Looping BackgroundThread (size=" + runnables.size() + ")");
 
+            // Loop through all enqueued BackgroundRunnables to be executed on the Framework
+            while (!runnables.isEmpty())
+            {
+                try
+                {
+                    BackgroundRunnable next = runnables.take();
+                    next.run(framework);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            // Sleep before next thread iteration
+            try
+            {
+                Thread.sleep(sleepDelay);
+            }
+            catch (InterruptedException exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public void queueRunnable(BackgroundRunnable runnable)
+    {
+        runnables.add(runnable);
     }
 
 }
