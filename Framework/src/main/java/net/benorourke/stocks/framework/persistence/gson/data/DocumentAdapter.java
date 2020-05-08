2afd56e9bd7841e44571cf7c7e4f79be12c0b04c
Ben O'Rourke <benorourke@users.noreply.github.com>
2020-05-08 04:21:27 +0100

    Add Unique Identifiers to Raw Data

--- Framework/src/main/java/net/benorourke/stocks/framework/persistence/gson/data/DocumentAdapter.java
+++ Framework/src/main/java/net/benorourke/stocks/framework/persistence/gson/data/DocumentAdapter.java
@@ -1,14 +1,13 @@
 package net.benorourke.stocks.framework.persistence.gson.data;
 
 import com.google.gson.*;
-import net.benorourke.stocks.framework.Framework;
 import net.benorourke.stocks.framework.persistence.gson.JsonAdapter;
-import net.benorourke.stocks.framework.series.data.impl.Document;
 import net.benorourke.stocks.framework.series.data.DocumentType;
-import net.benorourke.stocks.framework.util.DateUtil;
+import net.benorourke.stocks.framework.series.data.impl.Document;
 
 import java.lang.reflect.Type;
 import java.util.Date;
+import java.util.UUID;
 
 public class DocumentAdapter extends JsonAdapter<Document>
 {
@@ -17,6 +16,7 @@ public class DocumentAdapter extends JsonAdapter<Document>
     public JsonElement serialize(Document document, Type typeOfSrc, JsonSerializationContext context)
     {
         JsonObject result = new JsonObject();
+        result.add("id", new JsonPrimitive(document.getId().toString()));
         result.add("date", new JsonPrimitive(document.getDate().getTime()));
         result.add("content", new JsonPrimitive(document.getContent()));
         result.add("type", new JsonPrimitive(document.getDocumentType().toString()));
@@ -28,10 +28,11 @@ public class DocumentAdapter extends JsonAdapter<Document>
             throws JsonParseException
     {
         JsonObject object = json.getAsJsonObject();
+        UUID id = UUID.fromString(object.getAsJsonPrimitive("id").getAsString());
         Date date = new Date(object.getAsJsonPrimitive("date").getAsLong());
         String content = object.getAsJsonPrimitive("content").getAsString();
         DocumentType type = DocumentType.valueOf(object.getAsJsonPrimitive("type").getAsString());
-        return new Document(date, content, type);
+        return new Document(id, date, content, type);
     }
 
 }
--- Framework/src/main/java/net/benorourke/stocks/framework/persistence/gson/data/StockQuoteAdapter.java
+++ Framework/src/main/java/net/benorourke/stocks/framework/persistence/gson/data/StockQuoteAdapter.java
@@ -1,13 +1,12 @@
 package net.benorourke.stocks.framework.persistence.gson.data;
 
 import com.google.gson.*;
-import net.benorourke.stocks.framework.Framework;
 import net.benorourke.stocks.framework.persistence.gson.JsonAdapter;
 import net.benorourke.stocks.framework.series.data.impl.StockQuote;
-import net.benorourke.stocks.framework.util.DateUtil;
 
 import java.lang.reflect.Type;
 import java.util.Date;
+import java.util.UUID;
 
 public class StockQuoteAdapter extends JsonAdapter<StockQuote>
 {
@@ -16,6 +15,7 @@ public class StockQuoteAdapter extends JsonAdapter<StockQuote>
     public JsonElement serialize(StockQuote quote, Type typeOfSrc, JsonSerializationContext context)
     {
         JsonObject result = new JsonObject();
+        result.add("id", new JsonPrimitive(quote.getId().toString()));
         result.add("date", new JsonPrimitive(quote.getDate().getTime()));
         result.add("open", new JsonPrimitive(quote.getOpen()));
         result.add("close", new JsonPrimitive(quote.getClose()));
@@ -30,6 +30,7 @@ public class StockQuoteAdapter extends JsonAdapter<StockQuote>
             throws JsonParseException
     {
         JsonObject object = json.getAsJsonObject();
+        UUID id = UUID.fromString(object.getAsJsonPrimitive("id").getAsString());
         Date date = new Date(object.getAsJsonPrimitive("date").getAsLong());
         double open = object.getAsJsonPrimitive("open").getAsDouble();
         double close = object.getAsJsonPrimitive("close").getAsDouble();
@@ -37,9 +38,7 @@ public class StockQuoteAdapter extends JsonAdapter<StockQuote>
         double low = object.getAsJsonPrimitive("low").getAsDouble();
         long volume = object.getAsJsonPrimitive("volume").getAsLong();
 
-        Framework.debug("Deserialized Quote");
-
-        return new StockQuote(date, open, close, high, low, volume);
+        return new StockQuote(id, date, open, close, high, low, volume);
     }
 
 }
